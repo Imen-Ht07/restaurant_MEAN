822d05f2f569ae250e9bc81ae76cc4c5b7a840ec
Imen Hadj Taieb <115839915+Imen-Ht07@users.noreply.github.com>
2023-03-04 14:17:23 +0100

    Update database.js

--- RestoBack/database.js
+++ RestoBack/database.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://ImenHT:Bac.2020@cluster0.qjhwp.mongodb.net/resto")
+mongoose.connect("mongodb+srv://user:psw@cluster0.qjhwp.mongodb.net/resto")
 .then(bd => console.log("database connected")) 
 .catch(console.error());
